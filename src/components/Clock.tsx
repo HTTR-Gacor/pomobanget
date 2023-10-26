54a6f5a6166e1b9016b4b49bd261611fdbc51988
Muhammad Tarreq Maulana <tarreq@Muhammads-MacBook-Air-5.local>
2023-10-26 14:53:29 +0700

    style: clock shaddow

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,6 +1,6 @@
 export const Clock = () => {
   return (
-    <div className="bg-white/25 opacity-80 py-2 px-16 rounded-2xl my-10">
+    <div className="bg-white/25 shadow-xl opacity-80 py-2 px-16 rounded-2xl my-10">
       <p className="text-[8rem] font-bold">
         25:00
       </p>

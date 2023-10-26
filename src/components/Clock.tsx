aee9dad16617957dd7ee002e42ab7b21af3bf9a6
Muhammad Tarreq Maulana <tarreq@Muhammads-MacBook-Air-5.local>
2023-10-26 15:38:54 +0700

    style: start button color

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -2,7 +2,7 @@ import {IoMdRefresh} from "react-icons/io"
 
 export const Clock = () => {
   return (
-    <div>
+    <div className="flex flex-col items-center">
       <div className="bg-white/25 shadow-xl relative opacity-80 py-2 px-16 rounded-2xl my-10">
         <button className="absolute right-5 top-4">
           <IoMdRefresh size={19} />
@@ -11,8 +11,8 @@ export const Clock = () => {
           25:00
         </p>
       </div>
-      <button className="bg-white/25 shadow-xl opacity-80 py-5 px-10 w-3/4 rounded-2xl my-3">
-        <p>
+      <button className="bg-primary shadow-xl opacity-80 py-5 px-10 w-1/3 rounded-2xl my-3">
+        <p className="text-[2rem] font-bold text-secondary">
           Start
         </p>
       </button>

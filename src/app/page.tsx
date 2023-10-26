8259c8b22ac556eeb00357b364eeb3a9f8c458b3
Muhammad Tarreq Maulana <tarreq@Muhammads-MacBook-Air-5.local>
2023-10-26 14:44:16 +0700

    feat: clock container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import { Clock } from "@/components/Clock";
 
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center py-20">
       <h1 className="text-3xl font-bold drop-shadow-md">
-        POMOBANGET 
+        POMOBANGET
       </h1>
+      <Clock/>
     </div>
 
   )
---
+++ src/components/Clock.tsx
@@ -0,0 +1,9 @@
+export const Clock = () => {
+  return (
+    <div className="bg-white/25 opacity-80 py-2 px-16 rounded-2xl my-10">
+      <p className="text-[8rem] font-bold">
+        25:00
+      </p>
+    </div>
+  )
+}

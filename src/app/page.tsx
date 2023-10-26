8ac9e821f827e4ef58e118072426d137ca0ed298
Muhammad Tarreq Maulana <tarreq@Muhammads-MacBook-Air-5.local>
2023-10-26 14:52:00 +0700

    feat: desc by httr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,11 @@ import { Clock } from "@/components/Clock";
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center py-20">
-      <h1 className="text-3xl font-bold drop-shadow-md">
-        POMOBANGET
+      <h1 className="text-3xl drop-shadow-md">
+        <span className="font-extrabold tracking-wider">POMO</span>BANGET
+      </h1>
+      <h1 className="text-sm font-bold opacity-60">
+        by HTTR
       </h1>
       <Clock/>
     </div>
